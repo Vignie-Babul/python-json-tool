8a18c14b19b58d0db3c7eb0c66884d6722101d1c
Vignie <vigniebabul.dev@gmail.com>
2022-11-06 09:40:58 +0200

    Path fix

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ import json
 
 
 def save(data: dict, file_name: str, path='assets/data/'):
-	with open(f'assets/data/{file_name}.json', 'w') as file:
+	with open(f'{path}{file_name}.json', 'w') as file:
 		json.dump(data, file)
 
 def read(file_name: str, path='assets/data/') -> dict:
-	with open(f'assets/data/{file_name}.json', 'r') as file:
+	with open(f'{path}{file_name}.json', 'r') as file:
 		data = json.load(file)
 		return data
