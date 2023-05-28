e11c726e9e8295a07c0dea97d8e71b8d881ae429
Vignie Babul <vigniebabul.dev@gmail.com>
2023-05-28 11:41:24 +0200

    Update data.py

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ import json
 
 
 def save(data: dict, path: str):
-	with open(str(path), 'w') as file:
+	with open(path, 'w') as file:
 		json.dump(data, file)
 
 def read(path: str) -> dict:
-	with open(str(path), 'r') as file:
+	with open(path, 'r') as file:
 		data = json.load(file)
 		return data
